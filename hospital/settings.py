b5327a1940729246979b449ced44145aba12a26a
phamkhanhtrang <trangpk.22it@vku.udn.vn>
2025-10-16 18:20:04 +0700

    Update settings.py for database connection

--- hospital/settings.py
+++ hospital/settings.py
@@ -98,16 +98,18 @@ WSGI_APPLICATION = "hospital.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "appointment_db",
-        "USER": "admin",
-        "PASSWORD": "28042004",
-        "HOST": "database-web.cfggo8wow92g.ap-southeast-2.rds.amazonaws.com",
-        "PORT": "3306",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "3306"),
     }
 }
+
 
 
 # Password validation

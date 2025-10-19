2562f96283012177929fc868bc133552f51aea1d
phamkhanhtrang <trangpk.22it@vku.udn.vn>
2025-10-19 14:46:38 +0700

    Update settings.py for build

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
+#!/usr/bin/env bash
+# Dừng script nếu có lỗi
 set -o errexit
 
+# Cài đặt dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic; --no-input
-python manage.py makemigrations; --noinput
-python manage.py migrate; --noinput
+# Thu thập static files mà không hỏi lại
+python manage.py collectstatic --no-input
+
+# Tạo migrations nếu có thay đổi trong models
+python manage.py makemigrations --noinput
+
+# Áp dụng các migrations vào database
+python manage.py migrate --noinput

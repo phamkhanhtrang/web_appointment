243f22cd74c008609a52e215e315800dfbe96c47
phamkhanhtrang <trangpk.22it@vku.udn.vn>
2025-12-15 00:45:12 +0700

    6_2

--- admin/views.py
+++ admin/views.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import json
+from django.utils.timezone import localtime
 from django.template import loader
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
